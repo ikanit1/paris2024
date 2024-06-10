b3c4f98dc56e090fd88ccf7925d02d9dcbc07d2e
ikanit1 <nick040731@gmail.com>
2024-06-11 04:03:43 +0500

    Сделаны путь огня с чего до чего, эстафета. Добавлена вкладка празднования

--- app/src/main/java/com/example/paris2200/CelebrationActivity.java
+++ app/src/main/java/com/example/paris2200/CelebrationActivity.java
@@ -1,6 +1,10 @@
 package com.example.paris2200;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CelebrationActivity extends AppCompatActivity {
@@ -8,5 +12,39 @@ public class CelebrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_celebration);
+
+        // Информация о праздновании Олимпийских игр в Париже 2024
+        String celebrationInfo = "Париж 2024 - это грандиозное событие, которое превратит город в центр спорта и культуры. Ожидается, что олимпийские и паралимпийские игры привлекут миллионы зрителей и болельщиков со всего мира.";
+
+        celebrationInfo += "\n\nГлавные мероприятия:\n";
+        celebrationInfo += "- Церемония открытия Олимпийских игр: 26 июля 2024 года\n";
+        celebrationInfo += "- Церемония закрытия Олимпийских игр: 11 августа 2024 года\n";
+        celebrationInfo += "- Церемония открытия Паралимпийских игр: 28 августа 2024 года\n";
+        celebrationInfo += "- Церемония закрытия Паралимпийских игр: 8 сентября 2024 года\n";
+
+        celebrationInfo += "\nПрисоединяйтесь к нам и наслаждайтесь незабываемыми моментами спортивного праздника в самом сердце Парижа!";
+
+        // Выделение важных дат в тексте
+        Spannable spannable = new SpannableString(celebrationInfo);
+        int startIndex = celebrationInfo.indexOf("26 июля 2024 года");
+        int endIndex = startIndex + "26 июля 2024 года".length();
+        spannable.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        startIndex = celebrationInfo.indexOf("11 августа 2024 года");
+        endIndex = startIndex + "11 августа 2024 года".length();
+        spannable.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        startIndex = celebrationInfo.indexOf("28 августа 2024 года");
+        endIndex = startIndex + "28 августа 2024 года".length();
+        spannable.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        startIndex = celebrationInfo.indexOf("8 сентября 2024 года");
+        endIndex = startIndex + "8 сентября 2024 года".length();
+        spannable.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Отображение информации о праздновании
+        TextView celebrationTextView = findViewById(R.id.celebrationTextView);
+        celebrationTextView.setText(spannable);
+
     }
 }
--- app/src/main/java/com/example/paris2200/TorchActivity.java
+++ app/src/main/java/com/example/paris2200/TorchActivity.java
@@ -1,12 +1,81 @@
 package com.example.paris2200;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TorchActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TorchActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_torch);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+        MapsInitializer.initialize(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        googleMap = map;
+
+        // Пример точек маршрута (координаты можно заменить на реальные)
+        List<LatLng> relayPath = Arrays.asList(
+                new LatLng(37.9838, 23.7275), // Афины, Греция
+                new LatLng(41.9028, 12.4964), // Рим, Италия
+                new LatLng(48.8566, 2.3522)   // Париж, Франция
+        );
+
+        // Добавление линий на карту
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .addAll(relayPath)
+                .color(0xFFFF0000) // Красный цвет
+                .width(5); // Ширина линии
+        googleMap.addPolyline(polylineOptions);
+
+        // Перемещение камеры на первый пункт маршрута
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(relayPath.get(0), 5f));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
